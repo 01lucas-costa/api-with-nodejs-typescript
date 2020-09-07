5540a503cdff5c0fbb9fb7a239706b0322f532fd
01lucas-costa <01lucascosta@gmail.com>
2020-09-07 02:02:29 -0300

    Configurando MongoDB com Node js e Typescript

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,39 @@
-import './util/module-alias';
-import { Server } from '@overnightjs/core';
-import bodyParser from 'body-parser';
-import { ForeCastController } from './controllers/forecast';
-import { Application } from 'express';
+import './util/module-alias'
+import { Server } from '@overnightjs/core'
+import bodyParser from 'body-parser'
+import { ForeCastController } from './controllers/forecast'
+import { Application } from 'express'
+import * as database from '@src/database'
 
 export class SetupServer extends Server {
    constructor(private port = 3000) {
       super();
    }
 
-   public init(): void {
-      this.setupExpress();
-      this.setupControllers();
+   public async init(): Promise<void> {
+      this.setupExpress()
+      this.setupControllers()
+      await this.databaseSetup()
    }
 
    private setupExpress(): void {
-      this.app.use(bodyParser.json());
+      this.app.use(bodyParser.json())
    }
 
    private setupControllers(): void {
-      const forecastController = new ForeCastController();
-      this.addControllers(forecastController);
+      const forecastController = new ForeCastController()
+      this.addControllers(forecastController)
+   }
+
+   private async databaseSetup(): Promise<void> {
+      await database.connect()
+   }
+
+   public async close(): Promise<void> {
+      await database.close()
    }
 
    public getApp(): Application {
-      return this.app;
+      return this.app
    }
 }
