7174f913ed1abbea0423b9420ea296593ba67450
01lucas-costa <01lucascosta@gmail.com>
2020-09-07 02:19:14 -0300

    Criando os testes funcionais para módulo de Beach

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import './util/module-alias'
 import { Server } from '@overnightjs/core'
 import bodyParser from 'body-parser'
 import { ForeCastController } from './controllers/forecast'
+import { BeachesController } from './controllers/beaches'
 import { Application } from 'express'
 import * as database from '@src/database'
 
@@ -22,7 +23,8 @@ export class SetupServer extends Server {
 
    private setupControllers(): void {
       const forecastController = new ForeCastController()
-      this.addControllers(forecastController)
+      const beachesController = new BeachesController()
+      this.addControllers([forecastController, beachesController])
    }
 
    private async databaseSetup(): Promise<void> {
